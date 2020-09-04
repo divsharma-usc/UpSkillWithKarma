77e4b36c038f25f99b2ce299661dc887dee6782d
= <divyansh.sharma@hsbc.co.in>
2020-09-04 22:12:33 +0530

    enaballed swagger

---
+++ hackademy/API/src/main/java/com/noobHack/karma/Config/SwaggerConfiguration.java
@@ -0,0 +1,31 @@
+package com.noobHack.karma.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(Boolean.TRUE)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.noobHack.karma.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
